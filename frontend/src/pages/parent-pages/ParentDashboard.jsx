dba46a9ca12a637060e1bc91aa954d3124a035a1
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-04-27 12:14:19 +0600

    dashboard + check result

--- frontend/src/pages/parent-pages/ParentDashboard.jsx
+++ frontend/src/pages/parent-pages/ParentDashboard.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  Container,
+  Card,
+  CardContent,
+  styled,
+} from "@mui/material";
 
-function ParentDashboard() {
-  return <div>ParentDashboard</div>;
-}
+const GrayCard = styled(Card)({
+  background: "#f0f0f0",
+});
+
+const ParentDashboard = () => {
+  const [loading, setLoading] = useState(false);
+  const requestTutor = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography
+        m={4}
+        fontFamily={"Poppins"}
+        variant="h4"
+        align="center"
+        gutterBottom
+      >
+        Parent Dashboard
+      </Typography>
+      <Box sx={{ backgroundColor: "white", padding: 2 }}>
+        <Card>
+          <CardContent>
+            <Typography variant="body1">
+              Our "Tutor of the Month, May 2023" is Mustafa Rafid Nibir (Tutor
+              ID: 96177) and our "Guardian of the Month, May 2023" is Ziaul
+              Hasan (Job ID: 104518). Heartiest congratulations to both of them;
+              we're glad to work with them.
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <Grid container spacing={2} sx={{ marginTop: 2 }}>
+          <Grid item>
+            <GrayCard>
+              <CardContent>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="body2">00</Typography>
+                  <Typography variant="body2">Pending Job</Typography>
+                </Box>
+              </CardContent>
+            </GrayCard>
+          </Grid>
+          <Grid item>
+            <GrayCard>
+              <CardContent>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="body2">00</Typography>
+                  <Typography variant="body2">Live Job</Typography>
+                </Box>
+              </CardContent>
+            </GrayCard>
+          </Grid>
+          <Grid item>
+            <GrayCard>
+              <CardContent>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="body2">00</Typography>
+                  <Typography variant="body2">Appointed Job</Typography>
+                </Box>
+              </CardContent>
+            </GrayCard>
+          </Grid>
+          <Grid item>
+            <GrayCard>
+              <CardContent>
+                <Box
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography variant="body2">00</Typography>
+                  <Typography variant="body2">Confirmed Job</Typography>
+                </Box>
+              </CardContent>
+            </GrayCard>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
 
 export default ParentDashboard;
--- frontend/src/pages/parent-pages/TutorResult.jsx
+++ frontend/src/pages/parent-pages/TutorResult.jsx
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+  Container,
+  Box,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { btnStyles2 } from "../../styles/btnStyles2";
 
-function TutorResult() {
-  return <div>TutorResult</div>;
-}
+const TutorResult = () => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const checkResult = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setResult("GPA 5.00");
+      setLoading(false);
+    }, 1500);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography
+        m={4}
+        fontFamily={"Poppins"}
+        variant="h4"
+        align="center"
+        gutterBottom
+      >
+        Check Tutor Result
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Select fullWidth defaultValue="Select Examination">
+            <MenuItem value="Select Examination">
+              <em>Select Examination</em>
+            </MenuItem>
+            <MenuItem value="Male">HSC</MenuItem>
+            <MenuItem value="Male">SSC</MenuItem>
+            <MenuItem value="Male">JSC</MenuItem>
+          </Select>
+        </Grid>
+        <Grid item xs={12}>
+          <Select fullWidth defaultValue="Select Year">
+            <MenuItem value="Select Year">
+              <em>Select Year</em>
+            </MenuItem>
+            <MenuItem value="Male">2020</MenuItem>
+            <MenuItem value="Male">2021</MenuItem>
+            <MenuItem value="Male">2022</MenuItem>
+            <MenuItem value="Male">2023</MenuItem>
+          </Select>
+        </Grid>
+        <Grid item xs={12}>
+          <Select fullWidth defaultValue="Select Year">
+            <MenuItem value="Select Year">
+              <em>Select Board</em>
+            </MenuItem>
+            <MenuItem value="Male">Dhaka</MenuItem>
+            <MenuItem value="Male">Chattogram</MenuItem>
+            <MenuItem value="Male">Khulna</MenuItem>
+            <MenuItem value="Male">Rajshahi</MenuItem>
+            <MenuItem value="Male">Sylhet</MenuItem>
+            <MenuItem value="Male">Barisal</MenuItem>
+            <MenuItem value="Male">Cumilla</MenuItem>
+          </Select>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Roll" fullWidth />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Reg. No" fullWidth />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Solve: 4+3" fullWidth />
+        </Grid>
+        {result && (
+          <Grid item xs={12}>
+            <Box p={3}>
+              <Typography fontFamily={"Poppins"} variant="h4" align="center">
+                Tutor Result: {result}
+              </Typography>
+            </Box>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <LoadingButton
+            loading={loading}
+            loadingPosition="start"
+            sx={btnStyles2}
+            onClick={checkResult}
+            variant="contained"
+            color="primary"
+          >
+            Check Result
+          </LoadingButton>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default TutorResult;
