6fe7c8c9301c69a3628edbac6c0bc364a534a90d
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-05-01 23:08:26 +0600

    login remember user

--- frontend/src/pages/landing-page/LandingPage.jsx
+++ frontend/src/pages/landing-page/LandingPage.jsx
@@ -5,9 +5,15 @@ import Footer from "../../components/footer/Footer";
 import Features from "../../components/heromain/Features";
 import Heromain from "../../components/heromain/Heromain";
 import TuitionTypes from "../../components/heromain/TuitionTypes";
+import { urls } from "../../constants/urls";
+import { Navigate } from "react-router-dom";
 
 const LandingPage = () => {
-  return (
+  return localStorage.getItem("auth-parent") ? (
+    <Navigate to={urls.PARENT_DASHBOARD} />
+  ) : localStorage.getItem("auth-tutor") ? (
+    <Navigate to={urls.TUTOR_DASHBOARD} />
+  ) : (
     <Box sx={{ m: 3 }}>
       <Header />
       <Heromain />
