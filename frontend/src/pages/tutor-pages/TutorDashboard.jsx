fb2a76fef9eee94a3ad2043cf1bcbf75c67bf36b
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-05-05 02:43:26 +0600

    dashboard done

--- frontend/src/pages/tutor-pages/TutorDashboard.jsx
+++ frontend/src/pages/tutor-pages/TutorDashboard.jsx
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Grid,
-  Typography,
-  Container,
-  Card,
-  CardContent,
-  styled,
-} from "@mui/material";
-
-const GrayCard = styled(Card)({
-  background: "#f0f0f0",
-});
+import React from "react";
+import { Box, Grid, Typography, Container, CardContent } from "@mui/material";
+import CustomCard from "../../styles/customCard";
 
 const TutorDashboard = () => {
-
   return (
     <Container maxWidth="sm">
       <Typography
@@ -24,76 +12,99 @@ const TutorDashboard = () => {
         align="center"
         gutterBottom
       >
-        Tutor Dashboard
+        <b>Tutor Dashboard</b>
       </Typography>
-      <Box sx={{ backgroundColor: "white", padding: 2 }}>
-        <Card>
+      <Box>
+        <CustomCard>
           <CardContent>
-            <Typography variant="body1">
+            <Typography variant="body1" fontFamily="Poppins">
+              <b>Notice Board</b>
+              <br />
               Our "Tutor of the Month, May 2023" is Mustafa Rafid Nibir (Tutor
               ID: 96177) and our "Guardian of the Month, May 2023" is Ziaul
               Hasan (Job ID: 104518). Heartiest congratulations to both of them;
               we're glad to work with them.
             </Typography>
           </CardContent>
-        </Card>
+        </CustomCard>
 
         <Grid container spacing={2} sx={{ marginTop: 2 }}>
           <Grid item>
-            <GrayCard>
+            <CustomCard>
               <CardContent>
                 <Box
                   sx={{
                     textAlign: "center",
                   }}
                 >
-                  <Typography variant="body2">00</Typography>
-                  <Typography variant="body2">Pending Job</Typography>
+                  <Typography variant="h4" fontFamily="Poppins">
+                    00
+                  </Typography>
+                  <Typography variant="body2" fontFamily="Poppins">
+                    Pending Job
+                  </Typography>
                 </Box>
               </CardContent>
-            </GrayCard>
+            </CustomCard>
           </Grid>
           <Grid item>
-            <GrayCard>
+            <CustomCard>
               <CardContent>
                 <Box
                   sx={{
                     textAlign: "center",
                   }}
                 >
-                  <Typography variant="body2">00</Typography>
-                  <Typography variant="body2">Live Job</Typography>
+                  <Typography variant="h4" fontFamily="Poppins">
+                    00
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    fontFamily="Poppins"
+                    ml={1.2}
+                    mr={1.2}
+                  >
+                    Live Job
+                  </Typography>
                 </Box>
               </CardContent>
-            </GrayCard>
+            </CustomCard>
           </Grid>
           <Grid item>
-            <GrayCard>
+            <CustomCard>
               <CardContent>
                 <Box
                   sx={{
                     textAlign: "center",
                   }}
                 >
-                  <Typography variant="body2">00</Typography>
-                  <Typography variant="body2">Appointed Job</Typography>
+                  <Typography variant="h4" fontFamily="Poppins">
+                    00
+                  </Typography>
+                  <Typography variant="body2" fontFamily="Poppins">
+                    Appointed Job
+                  </Typography>
                 </Box>
               </CardContent>
-            </GrayCard>
+            </CustomCard>
           </Grid>
           <Grid item>
-            <GrayCard>
+            <CustomCard>
               <CardContent>
                 <Box
                   sx={{
                     textAlign: "center",
                   }}
                 >
-                  <Typography variant="body2">00</Typography>
-                  <Typography variant="body2">Confirmed Job</Typography>
+                  <Typography variant="h4" fontFamily="Poppins">
+                    00
+                  </Typography>
+                  <Typography variant="body2" fontFamily="Poppins">
+                    Confirmed Job
+                  </Typography>
                 </Box>
               </CardContent>
-            </GrayCard>
+            </CustomCard>
           </Grid>
         </Grid>
       </Box>
